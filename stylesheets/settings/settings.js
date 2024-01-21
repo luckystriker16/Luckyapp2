9422f73848c42a63f1649ea224c5329c275fe7f6
luckystriker16 <75381524+luckystriker16@users.noreply.github.com>
2024-01-21 16:49:15 +0100

    Breiterer LuckySite support

--- stylesheets/settings/settings.js
+++ stylesheets/settings/settings.js
@@ -2,7 +2,7 @@ var settingsStorageName = "ahornSettings";
 
 function init_Settings(){ //ONLOAD
     ahorn.loadSettings = ()=>{
-        var settings_version = 16; //Muss bei änderung der Standardeinstellungen geändert werden.
+        var settings_version = 17; //Muss bei änderung der Standardeinstellungen geändert werden.
         if(localStorage.getItem(settingsStorageName)){
             if(settings_version > JSON.parse(localStorage.getItem(settingsStorageName)).settings_version){
                 localStorage.removeItem(settingsStorageName);
@@ -24,7 +24,7 @@ function init_Settings(){ //ONLOAD
                 rootHTMLDefaultLang: "de", //["SprachId" | undefined] Soll für eine HTML-Datei im Rootverzeichnis (/) den Linkmanager nutzen, muss hier eine Sprache dafür angegeben werden.
                 firstLoad: true, //Wird nach dem ersten vollen Seitenladevorgang false
                 downloadToLink: true, //Ist der Wert true, werden AutoLinks mit autoLink-type="download" als normaler Link geladen, wenn download_name im linkmanger nicht gesetzt oder "wartung" ist.
-                luckySiteUrlLangPos: [11,13] //Gibt die Position der Sprachinformationen in der URL an, sodasss sie mit substring() ausgelesen werden könnnen.
+                luckySiteUrlLangPos: [1,3], //Gibt die Position der Sprachinformationen in der URL an, sodasss sie mit substring() ausgelesen werden könnnen.
             }
             localStorage.setItem(settingsStorageName, JSON.stringify(ahorn.settings));
         }
